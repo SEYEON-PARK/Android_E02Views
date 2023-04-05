4d3e2b976e35e67c40783bdc76b0fbccf314de2f
SEYEON-PARK <fourleaf123@naver.com>
2023-04-05 13:32:28 +0900

    안드로이드 스튜디오 커밋 연습!

---
+++ app/src/main/java/net/skhu/MainActivity.java
@@ -0,0 +1,35 @@
+package net.skhu;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main); // 뷰 객체들로 액티버티를 채우는 것! 그 내용이 () 안에 있다!
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_signUp) {
+            Toast.makeText(this, "회원가입 메뉴 클릭", Toast.LENGTH_SHORT).show();
+            return true;
+        } else if (id == R.id.action_memo) {
+            Toast.makeText(this, "메모장 메뉴 클릭", Toast.LENGTH_LONG).show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
